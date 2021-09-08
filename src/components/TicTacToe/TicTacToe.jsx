4d6e480644c742d062e65f431da75379c53d289c
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-09-08 16:55:23 +0400

    feat(TicTacToe): add winnerLine array

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { loadState, saveState } from "../../Helpers/localStorage";
 
-
 export default function TicTacToe() {
   const [squares, setSquares] = useState(
     loadState("game") ? loadState("game") : Array(9).fill(null)
@@ -12,4 +11,15 @@ export default function TicTacToe() {
   );
   const [errorMessage, setErrorMessage] = useState("");
   const [localStorageMessage, setLocalStorageMessage] = useState("");
+
+  const winnerLine = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 }
