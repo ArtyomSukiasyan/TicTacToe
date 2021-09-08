e979aa06129c0d377b1994c20dc0f9e2604a8771
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-09-08 17:08:03 +0400

    feat(TicTacToe): add winner condition

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import { loadState, saveState } from "../../Helpers/localStorage";
+import Board from "../Board/Board";
+import styles from "./TicTacToe.module.css"
+
+const X = "X"
+const O = "O"
 
 export default function TicTacToe() {
   const [squares, setSquares] = useState(
@@ -23,5 +28,36 @@ export default function TicTacToe() {
     [2, 4, 6],
   ];
 
-  
+  const isWinner = () => {
+    const player = count % 2 === 0 ? X : O;
+
+    for (let i = 0; i < winnerLine.length; i++) {
+      const line = winnerLine[i];
+      if (
+        squares[line[0]] === player &&
+        squares[line[1]] === player &&
+        squares[line[2]] === player
+      ) {
+        setResult(`${player} win`);
+        saveState("winner ", `${player} win`);
+        return;
+      }
+    }
+
+    if (count === 8) {
+      setResult("Draw");
+      try {
+        saveState("winner", "Draw");
+      } catch {
+        setLocalStorageMessage("Impossible to save in your local storage");
+      }
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Board squares={squares} />
+
+    </div>
+  );
 }
